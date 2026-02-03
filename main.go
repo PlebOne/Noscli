24e3e6c8d87adca191aa4a5df4eeba16f1b859f4
Tim <tim@pleb.one>
2026-02-03 12:24:49 -0600

    Redirect debug logs to ~/.config/noscli/debug.log
    TUI apps hide console output, so logs now go to file.
    Users can tail -f the debug log to see what's happening:
    
      tail -f ~/.config/noscli/debug.log
    
    This will show all the NWC payment flow debugging info.

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/charmbracelet/bubbletea"
 	"noscli/pkg/tui"
 )
 
 func main() {
+	// Set up debug logging to file
+	home, err := os.UserHomeDir()
+	if err == nil {
+		logPath := filepath.Join(home, ".config", "noscli", "debug.log")
+		os.MkdirAll(filepath.Dir(logPath), 0700)
+		logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+		if err == nil {
+			log.SetOutput(logFile)
+			log.Printf("\n\n========== Noscli Started ==========")
+			defer logFile.Close()
+		}
+	}
+
 	m := tui.NewModel()
 	p := tea.NewProgram(m, tea.WithAltScreen())
 
